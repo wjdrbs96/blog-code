9f258a16b89714a63d0bcf9af698ddf188b871d4
Gyunny <wjdrbs966@naver.com>
2021-11-08 20:36:09 +0900

    PagingDefault add

--- JPA_Paging/src/main/java/com/paging/controller/PostController.java
+++ JPA_Paging/src/main/java/com/paging/controller/PostController.java
@@ -4,6 +4,7 @@ import com.paging.dto.PostResponseDTO;
 import com.paging.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/post")
-    public List<PostResponseDTO> find(Pageable pageable) {
+    public List<PostResponseDTO> find(@PageableDefault(size = 7) Pageable pageable) {
         return postService.findAll(pageable).getContent();
     }
 
